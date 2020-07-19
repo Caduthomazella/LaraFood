1c56a6ae0814c72a57fb0337161b3812d4fbcd7d
Carlos Eduardo Leal Thomazella <caduthomazella@hotmail.com>
2020-07-19 12:50:52 -0300

    criando rota resource e listando os perfis

---
+++ app/Http/Controllers/Admin/ACL/ProfileController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Admin\ACL;
+
+use App\Http\Controllers\Controller;
+use App\Models\Profile;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    protected $repository;
+
+    public function __construct(Profile $profile)
+    {
+        $this->repository = $profile;
+    }
+
+    public function index()
+    {
+        $profiles = $this->repository->paginate();
+
+        return view('admin.pages.profiles.index', compact('profiles'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
